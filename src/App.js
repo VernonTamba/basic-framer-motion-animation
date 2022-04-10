95726bd10b11b6fe4b07450bbe8334e4c8810567
VernonTamba <77598362+VernonTamba@users.noreply.github.com>
2022-04-10 21:32:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import images from "./Images";
+import { motion } from "framer-motion";
 
 function App() {
+  const [width, setWidth] = useState(0);
+
+  const itemRef = useRef();
+
+  // useEffect(() => {
+  //   console.log(itemRef.current.scrollWidth);
+  //   console.log(itemRef.current.offsetWidth * 3);
+  // }, []);
+
+  const goPrevious = (currentWidth) => {
+    if (currentWidth === 0) {
+      setWidth(width);
+    } else {
+      setWidth(width - itemRef.current.offsetWidth);
+    }
+  };
+
+  const goNext = (currentWidth) => {
+    if (currentWidth === itemRef.current.scrollWidth) {
+      setWidth(width);
+    } else {
+      setWidth(width + itemRef.current.offsetWidth);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="previous" onClick={() => goPrevious(width)}>
+          Previous
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="next" onClick={() => goNext(width)}>
+          Next
+        </div>
       </header>
+      <motion.div ref={itemRef} className="carousel">
+        <motion.div animate={{ x: -width }} className="carousel__inner">
+          {images.map((image, index) => {
+            return (
+              <motion.div className="item">
+                <motion.img
+                  key={index}
+                  transition={{ duration: 0.1 }}
+                  whileHover={{ scale: 1.05 }}
+                  src={image}
+                  alt={image}
+                />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,8 @@
+import image1 from "./img/desk.jpg";
+import image2 from "./img/error.jpg";
+import image3 from "./img/hand.jpg";
+import image4 from "./img/man.jpg";
+import image5 from "./img/vr.jpg";
+import image6 from "./img/vr-2.jpg";
+
+export default [image1, image2, image3, image4, image5, image6];
