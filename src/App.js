871bbc006f78e1a518074bff0b28fbc23d66c009
Vernon Tamba <tambzfantastico@gmail.com>
2022-05-20 14:45:55 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import images from "./Images";
 import { motion } from "framer-motion";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 function App() {
   const [width, setWidth] = useState(0);
+  const [visible, setVisible] = useState(0);
 
   const itemRef = useRef();
-
-  // useEffect(() => {
-  //   console.log(itemRef.current.scrollWidth);
-  //   console.log(itemRef.current.offsetWidth * 3);
-  // }, []);
 
   const goPrevious = (currentWidth) => {
     if (currentWidth === 0) {
@@ -23,41 +21,64 @@ function App() {
   };
 
   const goNext = (currentWidth) => {
-    if (currentWidth === itemRef.current.scrollWidth) {
+    if (
+      currentWidth ===
+      itemRef.current.offsetWidth + itemRef.current.scrollWidth
+    ) {
       setWidth(width);
     } else {
       setWidth(width + itemRef.current.offsetWidth);
     }
   };
 
+  useEffect(() => {
+    if (width === 0) {
+      setVisible(0);
+    } else if (width * 2 >= itemRef.current.scrollWidth && width !== 0) {
+      setVisible(2);
+    } else {
+      setVisible(1);
+    }
+  }, [width]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className="previous" onClick={() => goPrevious(width)}>
-          Previous
-        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="next" onClick={() => goNext(width)}>
-          Next
-        </div>
       </header>
-      <motion.div ref={itemRef} className="carousel">
-        <motion.div animate={{ x: -width }} className="carousel__inner">
-          {images.map((image, index) => {
-            return (
-              <motion.div className="item">
-                <motion.img
-                  key={index}
-                  transition={{ duration: 0.1 }}
-                  whileHover={{ scale: 1.05 }}
-                  src={image}
-                  alt={image}
-                />
-              </motion.div>
-            );
-          })}
+      <div className="App__mainDiv">
+        {visible !== 0 && (
+          <NavigateBeforeIcon
+            style={{ fontSize: 100 }}
+            className="navigateBefore"
+            onClick={() => goPrevious(width)}
+          />
+        )}
+        <motion.div ref={itemRef} className="carousel">
+          <motion.div animate={{ x: -width }} className="carousel__inner">
+            {images.map((image, index) => {
+              return (
+                <motion.div className="item">
+                  <motion.img
+                    key={index}
+                    transition={{ duration: 0.1 }}
+                    whileHover={{ scale: 1.05 }}
+                    src={image}
+                    alt={image}
+                  />
+                </motion.div>
+              );
+            })}
+          </motion.div>
         </motion.div>
-      </motion.div>
+        {visible !== 2 && (
+          <NavigateNextIcon
+            style={{ fontSize: 100 }}
+            className="navigateNext"
+            onClick={() => goNext(width)}
+          />
+        )}
+      </div>
     </div>
   );
 }
